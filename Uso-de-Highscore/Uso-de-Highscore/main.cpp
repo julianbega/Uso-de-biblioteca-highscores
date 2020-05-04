b375e5f6eeab0d57a8262263a45210a08f42c843
BegaJulian <Julian.begamilrad@gmail.com>
2020-05-04 16:59:01 -0300

    Open Window
    Abre una ventana vacía

--- Uso-de-Highscore/Uso-de-Highscore/main.cpp
+++ Uso-de-Highscore/Uso-de-Highscore/main.cpp
@@ -1,2 +1,19 @@
 #include <GL/glut.h>
 
+
+void Display() 
+{
+	glutSwapBuffers();
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(480, 360);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Highscore");
+	glutDisplayFunc(Display);
+	glutMainLoop();
+
+}
