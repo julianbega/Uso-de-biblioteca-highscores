e7d8194ce89f28731db8d578829ace9c01897618
BegaJulian <Julian.begamilrad@gmail.com>
2020-05-04 20:56:51 -0300

    Hardcode + AddNewScore
    Hardcodeo de los players y scores del Highscore
    Arreglo de la funcion AddNewScore (los parametros del .h y el .cpp eran diferentes)

--- Uso-de-Highscore/Uso-de-Highscore/main.cpp
+++ Uso-de-Highscore/Uso-de-Highscore/main.cpp
@@ -10,8 +10,19 @@ void Display()
 
 void main(int argc, char** argv)
 {
-	AddNewScore(Scores _highScore[_sizeList], string namePlayer, int new_score);
-
+	Scores* _highScore[_sizeList];
+	string namePlayer;
+	int new_score;
+	AddNewScore(_highScore[0], "Juli", 100);
+	AddNewScore(_highScore[1], "Tomi", 200);
+	AddNewScore(_highScore[2], "Elias", 300);
+	AddNewScore(_highScore[3], "Simon", 80);
+	AddNewScore(_highScore[4], "Fermin", 600);
+	AddNewScore(_highScore[5], "Maxi", 400);
+	AddNewScore(_highScore[6], "Juan Crzu", 250);
+	AddNewScore(_highScore[7], "Iker", 220);
+	AddNewScore(_highScore[8], "Lauti", 90);
+	AddNewScore(_highScore[9], "Lucio", 120);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(480, 360);
